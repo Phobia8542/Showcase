e089e6a23e706c10c672b58ae90f9c3e6f10b1aa
Phobia8542 <62720477+Phobia8542@users.noreply.github.com>
2022-07-20 17:00:38 -0400

    Add files via upload

---
+++ Hello.world.py
@@ -0,0 +1,5 @@
+print ("hello world")
+print ("This should count from 0 to 9")
+
+for m in range(10):
+    print (m)
---
+++ math.py
@@ -0,0 +1,13 @@
+print (2+1)
+print (7/4)
+print (7%4)
+print (5+5*10)
+n = 5
+b = 6
+print (n)
+print (n+b)
+print (n+10)
+my_taxes = 1900
+tax_percentage = 0.8
+my_return = my_taxes * tax_percentage
+print (my_return)
